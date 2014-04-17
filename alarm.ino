fe51338fb7d228436ba81f2e479fca8020541d04
luis <lrosario101@gmail.com>
2014-04-17 02:35:15 -0400

    initial commit

---
+++ alarm.ino
@@ -0,0 +1,60 @@
+/*
+
+alarm clock
+
+based on:
+Tone
+Time
+*/
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+  // Read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  
+  // Print out the value you read:
+//  Serial.print(sensorValue);
+//  Serial.println(voltage);
+  if(sensorValue > 0) {
+    Serial.println("touchh");
+//    Serial.print(sensorValue);
+  }
+  // Wait 100 milliseconds
+  delay(100);
+}
